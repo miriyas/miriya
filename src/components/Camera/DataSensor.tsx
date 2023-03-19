fac39b40e065ab1209851a101bfb0d6d79359386
Lee Jun Hyuk <miriya.lee@gmail.com>
2023-03-20 03:43:49 +0900

    Fix more

--- src/components/Camera/DataSensor.tsx
+++ src/components/Camera/DataSensor.tsx
@@ -1,3 +1,5 @@
+import cx from 'classnames';
+
 import { SENSOR_SIZE, SensorSizeTypes, CameraType } from '@/types/cameras.d';
 
 import styles from './Camera.module.scss';
@@ -5,12 +7,12 @@ import styles from './Camera.module.scss';
 const prettySensorSize = (sensorSize?: SensorSizeTypes) => {
   if (!sensorSize) return <p className={styles.grayBg}>APS-C</p>;
   return {
-    [SENSOR_SIZE.FF]: <p className={styles.redBg}>FullFrame</p>,
-    [SENSOR_SIZE.APSH]: <p className={styles.orangeBg}>APS-H</p>,
-    [SENSOR_SIZE.APSC]: <p className={styles.grayBg}>APS-C</p>,
-    [SENSOR_SIZE.C19]: <p className={styles.yellowBg}>1.9crop</p>,
-    [SENSOR_SIZE.C26]: <p className={styles.yellowBg}>2.6crop</p>,
-    [SENSOR_SIZE.I23]: <p className={styles.yellowBg}>2/3&quot;</p>,
+    [SENSOR_SIZE.FF]: <p className={cx(styles.sensor, styles.redBg)}>FullFrame</p>,
+    [SENSOR_SIZE.APSH]: <p className={cx(styles.sensor, styles.orangeBg)}>APS-H</p>,
+    [SENSOR_SIZE.APSC]: <p className={cx(styles.sensor, styles.grayBg)}>APS-C</p>,
+    [SENSOR_SIZE.C19]: <p className={cx(styles.sensor, styles.yellowBg)}>1.9crop</p>,
+    [SENSOR_SIZE.C26]: <p className={cx(styles.sensor, styles.yellowBg)}>2.6crop</p>,
+    [SENSOR_SIZE.I23]: <p className={cx(styles.sensor, styles.yellowBg)}>2/3&quot;</p>,
   }[sensorSize];
 };
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -25,7 +25,7 @@ const App = ({ Component, pageProps }: AppProps) => {
 
   return (
     <>
-      <Component {...pageProps} />{' '}
+      <Component {...pageProps} />
       <button type='button' onClick={onClickToTop} className={styles.toTop}>
         <p>위로</p>
       </button>
