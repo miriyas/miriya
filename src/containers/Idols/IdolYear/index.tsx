fac7296eaecec50f6c88cf7f75ffebef2f18225f
Lee Jun Hyuk <miriya.lee@gmail.com>
2023-03-23 21:23:53 +0900

    Fix: Build error

--- src/containers/Idols/IdolYear/index.tsx
+++ src/containers/Idols/IdolYear/index.tsx
@@ -7,8 +7,8 @@ import { IsotopesType } from '@/types';
 import { IdolType } from '@/types/idols.d';
 import Isotope from '@/libs/isotope-layout';
 import { useResponsive } from '@/hooks/useResponsive';
-
 import Idol from '@/containers/idols/Idol';
+
 import styles from './IdolYear.module.scss';
 
 interface Props {
