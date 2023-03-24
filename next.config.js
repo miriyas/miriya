68b33bbd8167e3011af00a5de40288ddd079448e
Lee Jun Hyuk <miriya.lee@gmail.com>
2023-03-24 20:14:25 +0900

    Update image cache rule 2

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['https://www.digicamdb.com', 'miriya.sgp1.cdn.digitaloceanspaces.com', 'localhost', '*'],
+    domains: ['https://www.digicamdb.com', 'miriya.sgp1.cdn.digitaloceanspaces.com'],
+    imageSizes: [200, 280],
     minimumCacheTTL: 86400,
   },
   env: {
@@ -12,37 +13,24 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: '/:all*(svg|jpg|png)',
+  //       locale: false,
+  //       headers: [
+  //         {
+  //           key: 'Cache-Control',
+  //           value: 'public, max-age=31536000',
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      use: [
-        {
-          loader: '@svgr/webpack',
-          options: {
-            svgoConfig: {
-              plugins: [
-                // {
-                //   name: 'preset-default', // Prevent remove viewbox
-                //   params: {
-                //     overrides: { removeViewBox: false },
-                //   },
-                // },
-                // {
-                //   name: 'cleanupIDs', // Prevent id collision
-                //   params: {
-                //     prefix: {
-                //       toString() {
-                //         this.counter = this.counter || 0;
-                //         return `id-${this.counter++}`;
-                //       },
-                //     },
-                //   },
-                // },
-              ],
-            },
-          },
-        },
-      ],
+      use: ['@svgr/webpack'],
     });
 
     return config;
