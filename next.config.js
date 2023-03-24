b67684e268bf6dfc867b1fc67d94dbc2daf9af60
Lee Jun Hyuk <miriya.lee@gmail.com>
2023-03-24 20:16:50 +0900

    Update image cache rule 3

--- next.config.js
+++ next.config.js
@@ -13,20 +13,20 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  // async headers() {
-  //   return [
-  //     {
-  //       source: '/:all*(svg|jpg|png)',
-  //       locale: false,
-  //       headers: [
-  //         {
-  //           key: 'Cache-Control',
-  //           value: 'public, max-age=31536000',
-  //         },
-  //       ],
-  //     },
-  //   ];
-  // },
+  async headers() {
+    return [
+      {
+        source: '/:all*(svg|jpg|png)',
+        locale: false,
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=86400',
+          },
+        ],
+      },
+    ];
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
