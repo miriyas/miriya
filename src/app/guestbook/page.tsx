e72fd77c818353c79b69612e2588e6f7f36ca680
Lee Jun Hyuk <miriya.lee@gmail.com>
2023-04-03 20:41:05 +0900

    ADD: 깃허브, GA카운터 데이터 캐시 설정

--- src/app/guestbook/page.tsx
+++ src/app/guestbook/page.tsx
@@ -2,14 +2,18 @@ import GuestBook from '@/containers/guestbook';
 import { GADataRow } from '@/types/guestbook';
 
 const GuestBookPage = async () => {
-  const gaDataRaw = await fetch('https://us-central1-miriyas.cloudfunctions.net/getGAdata');
+  const gaDataRaw = await fetch('https://us-central1-miriyas.cloudfunctions.net/getGAdata', {
+    next: { revalidate: 60 * 30 }, // 30분 캐시
+  });
   const gaDataRes = await gaDataRaw.json();
   const gaData = gaDataRes.data as GADataRow[];
 
   const total = gaData[0].metricValues.map((v) => Number(v.value));
   const today = gaData[1].metricValues.map((v) => Number(v.value));
 
-  const commitsRaw = await fetch('https://api.github.com/repos/miriyas/miriya/commits');
+  const commitsRaw = await fetch('https://api.github.com/repos/miriyas/miriya/commits', {
+    next: { revalidate: 60 * 10 }, // 10분 캐시
+  });
   const commitsData = await commitsRaw.json();
 
   return (
