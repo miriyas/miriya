85b63fc54d5352710e21a60487a2bf69f083c3c0
Lee Jun Hyuk <miriya.lee@gmail.com>
2023-04-09 22:27:36 +0900

    FIX: Loading reposition

--- src/app/pentax/loading.tsx
+++ src/app/pentax/loading.tsx
@@ -1,7 +1,14 @@
-import LoadingPage from '@/components/Loading/LoadingPage';
+import LoadingSpinner from '@/components/Loading';
+import styles from './layout.module.scss';
 
 const Loading = () => {
-  return <LoadingPage inComponent />;
+  return (
+    <main className={styles.pentaxes}>
+      <div className={styles.centering}>
+        <LoadingSpinner />
+      </div>
+    </main>
+  );
 };
 
 export default Loading;
