99dc9cb90ed60aad06bcc23b6802dc3f33117b4c
Lee Jun Hyuk <miriya.lee@gmail.com>
2023-04-16 16:22:31 +0900

    FIX: Env

--- next.config.js
+++ next.config.js
@@ -20,7 +20,7 @@ const nextConfig = {
     ],
   },
   env: {
-    NEXT_PUBLIC_BASE_FETCH_URL: 'http://localhost:3000',
+    NEXT_PUBLIC_BASE_FETCH_URL: process.env.NEXT_PUBLIC_BASE_FETCH_URL,
     NEXT_PUBLIC_GA_ID: 'G-BCXXRGTY78',
     NEXT_PUBLIC_CDN_URL: 'https://miriya.sgp1.cdn.digitaloceanspaces.com',
     NEXT_FIREBASE_API_KEY: 'AIzaSyBA-Zq6nUqit0bxiPQCkOYuyofaGs3Tmkw',
