2ac0d30769877600914978ce1d049864c2a95942
Lee Jun Hyuk <miriya.lee@gmail.com>
2023-04-22 21:37:36 +0900

    FIX: fix build

--- src/utils/db/index.ts
+++ src/utils/db/index.ts
@@ -1,12 +1,10 @@
 /* eslint-disable no-console */
 import admin, { ServiceAccount } from 'firebase-admin';
 
-import serviceAccount from './serviceAccountKey.json';
-
 if (!admin.apps.length) {
   try {
     admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount as ServiceAccount),
+      credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY ?? '') as ServiceAccount),
     });
   } catch (error) {
     console.log('Firebase admin initialization error', error);
