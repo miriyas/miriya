c82fcf91672b9656ace5be08c7ec48c359dd55bf
Lee Jun Hyuk <miriya.lee@gmail.com>
2023-05-14 04:27:50 +0900

    TEST: og image to basic example

--- src/app/blog/[postId]/opengraph-image.tsx
+++ src/app/blog/[postId]/opengraph-image.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import { ImageResponse } from 'next/server';
 
-import { getPost } from './utils';
+// import { getPost } from './utils';
 
 export const size = {
   width: 1200,
@@ -10,96 +10,120 @@ export const size = {
 export const contentType = 'image/png';
 export const runtime = 'edge';
 
-interface Props {
-  params: {
-    postId: string;
-  };
-}
+// interface Props {
+//   params: {
+//     postId: string;
+//   };
+// }
 
 // https://vercel.com/docs/concepts/functions/edge-functions/og-image-generation/og-image-examples#load-files-in-node.js-runtime
-const og = async ({ params: { postId } }: Props) => {
-  try {
-    const postData = await getPost(postId);
-    // woff2는 지원 안됨
-    const font = await fetch(
-      new URL(`${process.env.NEXT_PUBLIC_LOCAL_FETCH_URL}/fonts/SpoqaHanSansNeo-Bold.woff`),
-    ).then((res) => res.arrayBuffer());
+const og = async () => {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 128,
+          background: 'white',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          textAlign: 'center',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        Hello world!
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 600,
+    },
+  );
 
-    // zIndex 지원 안됨, 나중에 올라오는게 위에 올라간다.
-    return new ImageResponse(
-      (
-        <div
-          style={{
-            width: '100%',
-            height: '100%',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          <img
-            src={postData.hero ?? 'https://miriya.sgp1.cdn.digitaloceanspaces.com/mycar/hL0XAW5GmEpx7Vn5czGs.jpg'}
-            style={{
-              position: 'absolute',
-              top: 0,
-              right: 0,
-              bottom: 0,
-              left: 0,
-              width: '100%',
-              objectFit: 'cover',
-            }}
-            alt=''
-          />
-          <div
-            style={{
-              position: 'absolute',
-              top: 0,
-              right: 0,
-              bottom: 0,
-              left: 0,
-              backgroundColor: 'rgba(0 0 0, 0.3)',
-            }}
-          />
-          <div
-            style={{
-              width: '100%',
-              height: '100%',
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}
-          >
-            <p
-              style={{
-                fontSize: 92,
-                fontWeight: 500,
-                fontFamily: 'Spoqa',
-                color: 'white',
-                textAlign: 'center',
-              }}
-            >
-              {postData.title}
-            </p>
-          </div>
-        </div>
-      ),
-      {
-        ...size,
-        fonts: [
-          {
-            name: 'Spoqa',
-            data: font,
-            weight: 700,
-            style: 'normal',
-          },
-        ],
-      },
-    );
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.log(error);
-    return null;
-  }
+  // try {
+  //   const postData = await getPost(postId);
+
+  //   // woff2는 지원 안됨
+  //   const font = await fetch(
+  //     new URL(`${process.env.NEXT_PUBLIC_LOCAL_FETCH_URL}/fonts/SpoqaHanSansNeo-Bold.woff`),
+  //   ).then((res) => res.arrayBuffer());
+
+  //   // zIndex 지원 안됨, 나중에 올라오는게 위에 올라간다.
+  //   return new ImageResponse(
+  //     (
+  //       <div
+  //         style={{
+  //           width: '100%',
+  //           height: '100%',
+  //           display: 'flex',
+  //           alignItems: 'center',
+  //           justifyContent: 'center',
+  //         }}
+  //       >
+  //         <img
+  //           src={postData.hero ?? 'https://miriya.sgp1.cdn.digitaloceanspaces.com/mycar/hL0XAW5GmEpx7Vn5czGs.jpg'}
+  //           style={{
+  //             position: 'absolute',
+  //             top: 0,
+  //             right: 0,
+  //             bottom: 0,
+  //             left: 0,
+  //             width: '100%',
+  //             objectFit: 'cover',
+  //           }}
+  //           alt=''
+  //         />
+  //         <div
+  //           style={{
+  //             position: 'absolute',
+  //             top: 0,
+  //             right: 0,
+  //             bottom: 0,
+  //             left: 0,
+  //             backgroundColor: 'rgba(0 0 0, 0.3)',
+  //           }}
+  //         />
+  //         <div
+  //           style={{
+  //             width: '100%',
+  //             height: '100%',
+  //             display: 'flex',
+  //             alignItems: 'center',
+  //             justifyContent: 'center',
+  //           }}
+  //         >
+  //           <p
+  //             style={{
+  //               fontSize: 92,
+  //               fontWeight: 500,
+  //               fontFamily: 'Spoqa',
+  //               color: 'white',
+  //               textAlign: 'center',
+  //             }}
+  //           >
+  //             {postData.title}
+  //           </p>
+  //         </div>
+  //       </div>
+  //     ),
+  //     {
+  //       ...size,
+  //       fonts: [
+  //         {
+  //           name: 'Spoqa',
+  //           data: font,
+  //           weight: 700,
+  //           style: 'normal',
+  //         },
+  //       ],
+  //     },
+  //   );
+  // } catch (error) {
+  //   // eslint-disable-next-line no-console
+  //   console.log(error);
+  //   return null;
+  // }
 };
 
 export default og;
