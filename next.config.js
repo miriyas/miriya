f9cd41a325346d7801a3ed58e7487c5b50bec0d0
Lee Jun Hyuk <miriya.lee@gmail.com>
2023-05-22 03:58:05 +0900

    ADD: Disable Sentry when dev

--- next.config.js
+++ next.config.js
@@ -108,35 +108,38 @@ module.exports = withBundleAnalyzer(nextConfig);
 
 // Inected Content via Sentry Wizard Below
 
-module.exports = withSentryConfig(
-  module.exports,
-  {
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options
+module.exports =
+  process.env.ENVIRONMENT === 'production'
+    ? withSentryConfig(
+        module.exports,
+        {
+          // For all available options, see:
+          // https://github.com/getsentry/sentry-webpack-plugin#options
 
-    // Suppresses source map uploading logs during build
-    silent: true,
+          // Suppresses source map uploading logs during build
+          silent: true,
 
-    org: 'miriya',
-    project: 'miriya',
-  },
-  {
-    // For all available options, see:
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+          org: 'miriya',
+          project: 'miriya',
+        },
+        {
+          // For all available options, see:
+          // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-    // Upload a larger set of source maps for prettier stack traces (increases build time)
-    widenClientFileUpload: true,
+          // Upload a larger set of source maps for prettier stack traces (increases build time)
+          widenClientFileUpload: true,
 
-    // Transpiles SDK to be compatible with IE11 (increases bundle size)
-    transpileClientSDK: true,
+          // Transpiles SDK to be compatible with IE11 (increases bundle size)
+          transpileClientSDK: true,
 
-    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
-    tunnelRoute: '/monitoring',
+          // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+          tunnelRoute: '/monitoring',
 
-    // Hides source maps from generated client bundles
-    hideSourceMaps: true,
+          // Hides source maps from generated client bundles
+          hideSourceMaps: true,
 
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
-    disableLogger: true,
-  },
-);
+          // Automatically tree-shake Sentry logger statements to reduce bundle size
+          disableLogger: true,
+        },
+      )
+    : module.exports;
