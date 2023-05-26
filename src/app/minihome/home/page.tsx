36837c8e6eb6b6e7e2b35634547455175793afac
Lee Jun Hyuk <miriya.lee@gmail.com>
2023-05-27 04:02:19 +0900

    ADD: 커밋 목록에 옥토킷 사용하여 제한 해제

--- src/app/minihome/home/page.tsx
+++ src/app/minihome/home/page.tsx
@@ -1,20 +1,11 @@
+import { Octokit } from 'octokit';
+
 import TabHome from '@/containers/minihome/TabHome';
 
 const MinihomeHomePage = async () => {
-  // 사용자당 1시간에 60번 제한이 있다. 풀 수 있지만 따로 인증은 귀찮아서...
-  // 어차피 한시간에 60번 새로고침 할 경우 그 사용자에게만 안보이게 됨.
-  const commitsData = await fetch('https://api.github.com/repos/miriyas/miriya/commits', {
-    next: { revalidate: 60 * 10 }, // 10분 캐시
-  })
-    .then((res) => {
-      if (!res.ok) {
-        return Promise.reject();
-      }
-      return res.json();
-    })
-    .catch(() => {
-      return [];
-    });
+  // Octokit을 쓰지 않고 바로 API 때리면 사용자당 1시간에 60번 제한이 있다.
+  const octokit = new Octokit({ auth: process.env.GITHUB_ACCESS_TOKEN });
+  const { data: commitsData } = await octokit.request('GET https://api.github.com/repos/miriyas/miriya/commits');
 
   return <TabHome commitsData={commitsData} />;
 };
