eda3b883861eb787e8abd04a54d94acc8ecc6d53
Lee Jun Hyuk <miriya.lee@gmail.com>
2023-05-26 23:33:22 +0900

    ADD: idol crawl page to SSG

--- src/app/idols/crawl/page.tsx
+++ src/app/idols/crawl/page.tsx
@@ -1,1 +1,51 @@
-export { default } from '@/containers/idols/Crawl';
+import { CrawlGeneration } from '@/types/idols.d';
+import { crawlIdolData } from '@/services/idols';
+
+import styles from './index.module.scss';
+import ExternalLink from '@/components/ExternalLink';
+
+const CrawlIdolPage = async () => {
+  const crawlData = await crawlIdolData()
+    .then((res) => res.data)
+    .catch(() => []);
+
+  return (
+    <main className={styles.idolCrawl}>
+      <div>
+        {Object.keys(crawlData).map((genName) => {
+          const generation = crawlData[genName] as CrawlGeneration;
+          return (
+            <table key={genName}>
+              <thead>
+                <tr>
+                  <td colSpan={2}>{genName}</td>
+                </tr>
+              </thead>
+              <tbody>
+                {Object.keys(generation).map((categoryName) => {
+                  const idols = generation[categoryName];
+                  return (
+                    <tr key={categoryName}>
+                      <td>{categoryName}</td>
+                      <td>
+                        {idols.map((idol) => {
+                          return (
+                            <ExternalLink key={idol.name} href={`https://namu.wiki${idol.url}`}>
+                              {idol.name}
+                            </ExternalLink>
+                          );
+                        })}
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          );
+        })}
+      </div>
+    </main>
+  );
+};
+
+export default CrawlIdolPage;
