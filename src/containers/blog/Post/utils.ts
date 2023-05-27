568f3741f2a50b06e5fe0314a7b6816afb99bd63
Lee Jun Hyuk <miriya.lee@gmail.com>
2023-05-28 04:57:16 +0900

    ADD: 블로그 이미지 확대기능

--- src/containers/blog/Post/utils.ts
+++ src/containers/blog/Post/utils.ts
@@ -1,3 +1,28 @@
 export const imageOptimize = (str: string) => {
   return str.replaceAll('miriya.sgp1.cdn.digitaloceanspaces.com', 'ik.imagekit.io/miriya/tr:w-1200,c-at_max');
 };
+
+export const imageExpandable = (rawString: string) => {
+  let processed = rawString;
+  const regex = /(?:[*#])|<img src="(.+?)">/g;
+  const matchedArray = [...processed.matchAll(regex)];
+  matchedArray.forEach((arr) => {
+    const original = arr[0];
+    const content = arr[1];
+
+    if (!content) return;
+    const [src, alt] = content.split('" alt="');
+    const originalSrc = src.replace([...src.matchAll(/tr:(.*?)\//g)][0][0], '');
+    processed = processed.replace(
+      original,
+      `<details class='imageExpandable'><summary><img src='${src}' alt='${alt}' class='summaryImg' /></summary><img src='${originalSrc}' alt='${alt}' class='detailsImg' /></details>`,
+    );
+  });
+  return processed;
+};
+
+export const bodyProcess = (rawString: string) => {
+  const optimized = imageOptimize(rawString);
+  const expandable = imageExpandable(optimized);
+  return expandable;
+};
