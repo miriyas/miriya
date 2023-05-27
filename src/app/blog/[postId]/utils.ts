9879c3b7579c773b818321fcb9014dfcf7c8f879
Lee Jun Hyuk <miriya.lee@gmail.com>
2023-05-27 09:08:48 +0900

    FIX: og:image

--- src/app/blog/[postId]/utils.ts
+++ src/app/blog/[postId]/utils.ts
@@ -1,9 +1,17 @@
 import { renderMarkdown } from '@/utils/blog';
-import { getBlogPostAPI } from '@/services/blog';
 import { FBBlogPost } from '@/types/blog.d';
+import { fetchClient } from '@/services/apiClient';
 
 export const getPost = async (postId: string): Promise<FBBlogPost> => {
-  const postData = await getBlogPostAPI(postId).then((res) => res.data);
+  // Axios 쓰면 안된다! og:image용 서비스워커는 fetch만 가능함 ㅠㅠ
+  // https://stackoverflow.com/questions/75280544/uncaught-in-promise-error-error-adapter-http-is-not-available-in-the-build
+  const postData = await fetchClient(`/blog/posts/${postId}`).then((res) => {
+    if (!res.ok) {
+      throw Error(res.statusText);
+    }
+    return res.json();
+  });
+
   const body = await renderMarkdown(postData.body);
   return {
     ...postData,
--- src/containers/blog/Post/utils.ts
+++ src/containers/blog/Post/utils.ts
@@ -1,5 +1,3 @@
 export const imageOptimize = (str: string) => {
-  return str
-    .replaceAll('miriya.sgp1.cdn.digitaloceanspaces.com', 'ik.imagekit.io/miriya/tr:w-1200,c-at_max')
-    .replaceAll(/(?<=<img src=")(.*)(?=" )/g, '$1?width=1200&fit=max');
+  return str.replaceAll('miriya.sgp1.cdn.digitaloceanspaces.com', 'ik.imagekit.io/miriya/tr:w-1200,c-at_max');
 };
