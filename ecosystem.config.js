4d91e4068397b0d8018f3c433a349e496166a00b
AlpoxDev <alpoxdev@gmail.com>
2023-06-09 14:21:55 +0900

    (fix) ecosystem.config.js exec_mode fork

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'frontend',
-      script: 'yarn start',
+      script: 'PORT=80 yarn run next start',
       node_args: '--max-old-space-size=1024',
       exec_mode: 'fork',
       instances: 1,
