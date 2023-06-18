1bd0420e0cdb913b93143c83c646560a55e5ad8a
Lee Jun Hyuk <miriya.lee@gmail.com>
2023-06-18 14:46:24 +0900

    FIX: m2 folder structure

---
+++ src/containers/m2day/Contents/Post/Functions.tsx
@@ -0,0 +1,101 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
+import cx from 'clsx';
+
+import useAuth from '@/hooks/useAuth';
+import useM2day from '../../useM2day';
+import { M2PostType } from '@/types/m2day.d';
+import { LIKE_TARGET_CATEGORY } from '@/types/likes.d';
+import { deleteLikeAPI, postLikeAPI } from '@/services/likes';
+
+import styles from './index.module.scss';
+
+interface Props {
+  post: M2PostType;
+}
+
+const Functions = ({ post }: Props) => {
+  const { id, likes, comments } = post;
+  const { user, showLoginModalWhenLoggedOut } = useAuth();
+  const [isLoading, setIsLoading] = useState(false);
+  const [likedByMe, setLikedByMe] = useState(false); // optimistic
+
+  const { refetchPosts, isLoadingPosts } = useM2day();
+
+  useEffect(() => {
+    setLikedByMe(likes.map((like) => like.authorId).includes(user?.uid ?? ''));
+  }, [isLoadingPosts, likes, user?.uid]);
+
+  const doLike = () => {
+    setLikedByMe(true);
+    setIsLoading(true);
+    postLikeAPI({
+      targetCategory: LIKE_TARGET_CATEGORY.M2_POST,
+      targetId: id,
+    })
+      .then(() => {
+        refetchPosts();
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
+  const doUnLike = () => {
+    setLikedByMe(false);
+    setIsLoading(true);
+    deleteLikeAPI({
+      targetCategory: LIKE_TARGET_CATEGORY.M2_POST,
+      targetId: id,
+    })
+      .then(() => {
+        refetchPosts();
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
+  const onClickMeToo = () => {
+    showLoginModalWhenLoggedOut(() => {
+      if (likedByMe) {
+        doUnLike();
+        return;
+      }
+      doLike();
+    });
+  };
+
+  const onClickComment = () => {
+    // console.log('me2');
+  };
+
+  return (
+    <div className={styles.functions}>
+      <button
+        type='button'
+        onClick={onClickMeToo}
+        className={cx(styles.me2, { [styles.active]: likedByMe })}
+        disabled={isLoading}
+      >
+        {likedByMe ? (
+          <Image className={styles.logo} src='/images/m2day/me2invert.png' width={50} height={58} alt='' />
+        ) : (
+          <Image className={styles.logo} src='/images/m2day/me2.png' width={50} height={58} alt='' />
+        )}
+        미투
+      </button>
+      <button type='button' onClick={onClickMeToo} className={styles.me2Count}>
+        {likes.length}
+      </button>
+      <button type='button' onClick={onClickComment} className={styles.comment}>
+        {comments.length > 0 ? '댓글' : '댓글달기'}
+        {comments.length > 0 && <span>{comments.length}</span>}
+      </button>
+    </div>
+  );
+};
+
+export default Functions;
--- src/containers/m2day/Contents/Post/index.tsx
+++ src/containers/m2day/Contents/Post/index.tsx
@@ -1,18 +1,12 @@
 'use client';
 
-/* eslint-disable react/no-danger */
-import { useEffect, useState } from 'react';
-import Image from 'next/image';
 import { useAtom } from 'jotai';
-import cx from 'clsx';
 
-import useAuth from '@/hooks/useAuth';
 import useM2day from '../../useM2day';
 import { M2PostType } from '@/types/m2day.d';
-import { LIKE_TARGET_CATEGORY } from '@/types/likes.d';
 import { getTimeDiffText } from '@/utils/date';
 import { editModeAtom } from '@/containers/m2day/states';
-import { deleteLikeAPI, postLikeAPI } from '@/services/likes';
+import Functions from '@/containers/m2day/Contents/Post/Functions';
 
 import LeftIcon from './LeftIcon';
 import Content from './Content';
@@ -26,64 +20,13 @@ interface Props {
 }
 
 const M2Post = ({ post }: Props) => {
-  const { id, createdAt, content, likes, comments } = post;
-  const { user, showLoginModalWhenLoggedOut } = useAuth();
+  const { createdAt, content } = post;
   const [editMode, setEditMode] = useAtom(editModeAtom);
-  const [isLoading, setIsLoading] = useState(false);
-  const [likedByMe, setLikedByMe] = useState(false); // optimistic
 
-  const { onClickDelete, refetchPosts, isLoadingPosts } = useM2day();
-
-  useEffect(() => {
-    setLikedByMe(likes.map((like) => like.authorId).includes(user?.uid ?? ''));
-  }, [isLoadingPosts, likes, user?.uid]);
+  const { onClickDelete } = useM2day();
 
   const onClickEdit = () => {
     setEditMode(post.id);
-  };
-
-  const doLike = () => {
-    setLikedByMe(true);
-    setIsLoading(true);
-    postLikeAPI({
-      targetCategory: LIKE_TARGET_CATEGORY.M2_POST,
-      targetId: id,
-    })
-      .then(() => {
-        refetchPosts();
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-  };
-
-  const doUnLike = () => {
-    setLikedByMe(false);
-    setIsLoading(true);
-    deleteLikeAPI({
-      targetCategory: LIKE_TARGET_CATEGORY.M2_POST,
-      targetId: id,
-    })
-      .then(() => {
-        refetchPosts();
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-  };
-
-  const onClickMeToo = () => {
-    showLoginModalWhenLoggedOut(() => {
-      if (likedByMe) {
-        doUnLike();
-        return;
-      }
-      doLike();
-    });
-  };
-
-  const onClickComment = () => {
-    // console.log('me2');
   };
 
   return (
@@ -106,28 +49,7 @@ const M2Post = ({ post }: Props) => {
             </div>
             <PannelYoutube postId={post.id} />
             <PannelImage postId={post.id} />
-            <div className={styles.functions}>
-              <button
-                type='button'
-                onClick={onClickMeToo}
-                className={cx(styles.me2, { [styles.active]: likedByMe })}
-                disabled={isLoading}
-              >
-                {likedByMe ? (
-                  <Image className={styles.logo} src='/images/m2day/me2invert.png' width={50} height={58} alt='' />
-                ) : (
-                  <Image className={styles.logo} src='/images/m2day/me2.png' width={50} height={58} alt='' />
-                )}
-                미투
-              </button>
-              <button type='button' onClick={onClickMeToo} className={styles.me2Count}>
-                {likes.length}
-              </button>
-              <button type='button' onClick={onClickComment} className={styles.comment}>
-                {comments.length > 0 ? '댓글' : '댓글달기'}
-                {comments.length > 0 && <span>{comments.length}</span>}
-              </button>
-            </div>
+            <Functions post={post} />
           </>
         )}
       </div>
