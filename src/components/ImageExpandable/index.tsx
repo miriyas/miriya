ebf25099c7c7330b401b8e3604c3d551c8bf22a6
Lee Jun Hyuk <miriya.lee@gmail.com>
2023-06-23 02:02:17 +0900

    ADD: VTA for M2day post image pannel

--- src/components/ImageExpandable/index.tsx
+++ src/components/ImageExpandable/index.tsx
@@ -9,17 +9,18 @@ interface Props {
   height?: number;
   alt?: string;
   ignoreDimension?: boolean;
+  summaryId?: string;
 }
 
 /** 순수 CSS로 만든 간단한 확대/축소 기능 */
-const ImageExpandable = ({ src, alt, width, height, ignoreDimension }: Props) => {
+const ImageExpandable = ({ src, alt, width, height, ignoreDimension, summaryId }: Props) => {
   return (
     <details className={styles.imageExpandable}>
       <summary>
         {ignoreDimension ? (
-          <img src={src} alt={alt ?? ''} className={styles.summaryImg} />
+          <img src={src} alt={alt ?? ''} className={styles.summaryImg} id={summaryId} />
         ) : (
-          <Image src={src} alt={alt ?? ''} width={width} height={height} className={styles.summaryImg} />
+          <Image src={src} alt={alt ?? ''} width={width} height={height} className={styles.summaryImg} id={summaryId} />
         )}
       </summary>
       {ignoreDimension ? (
--- src/containers/m2day/Contents/Post/PannelImage.tsx
+++ src/containers/m2day/Contents/Post/PannelImage.tsx
@@ -2,6 +2,9 @@
 import { MouseEventHandler, useEffect, useState } from 'react';
 import Image from 'next/image';
 import cx from 'clsx';
+import { flushSync } from 'react-dom';
+
+import { transitionHelper } from '@/utils/transition';
 
 import { IconChevronRight } from 'public/svgs';
 import ImageExpandable from '@/components/ImageExpandable';
@@ -15,6 +18,8 @@ interface Props {
 const PannelImage = ({ postId }: Props) => {
   const [selectedIdx, setSelectedIdx] = useState(0);
   const [imageUrls, setImageUrls] = useState<string[]>([]);
+  const summaryId = `${postId}-album-summary`;
+  const pannelThumbnails = `${postId}-album-pannelThumbnails`;
 
   useEffect(() => {
     const openImagePannel = (e: any) => {
@@ -32,16 +37,43 @@ const PannelImage = ({ postId }: Props) => {
     };
   }, [imageUrls.length, postId]);
 
+  const moveToImage = (targetIdx: number, thumbnail: HTMLImageElement | null) => {
+    document.querySelector<HTMLImageElement>(`#${summaryId}`)?.style.setProperty('view-transition-name', '');
+    thumbnail?.style.setProperty('view-transition-name', 'transition-m2-album');
+    transitionHelper({
+      updateDOM() {
+        flushSync(() => {
+          thumbnail?.style.setProperty('view-transition-name', '');
+          document
+            .querySelector<HTMLImageElement>(`#${summaryId}`)
+            ?.style.setProperty('view-transition-name', 'transition-m2-album');
+          setSelectedIdx(targetIdx);
+        });
+      },
+    }).finished.finally(() => {
+      document.querySelector<HTMLImageElement>(`#${summaryId}`)?.style.setProperty('view-transition-name', '');
+      thumbnail?.style.setProperty('view-transition-name', '');
+    });
+  };
+
   const onClickThumbnail: MouseEventHandler<HTMLButtonElement> = (e) => {
-    setSelectedIdx(Number(e.currentTarget.dataset.idx));
+    const targetIdx = Number(e.currentTarget.dataset.idx);
+    if (selectedIdx === targetIdx) return;
+    const thumbnail = e.currentTarget.querySelector('img'); // ref를 쓰면 더러워서..
+    moveToImage(targetIdx, thumbnail);
   };
 
   const onClickPrev = () => {
-    setSelectedIdx((prev) => Math.max(0, prev - 1));
+    const prevIdx = Math.max(0, selectedIdx - 1);
+    const thumbnail = document.querySelector<HTMLImageElement>(`#${pannelThumbnails} [data-idx='${prevIdx}'] img`);
+    moveToImage(prevIdx, thumbnail);
   };
 
   const onClickNext = () => {
-    setSelectedIdx((prev) => Math.min(imageUrls.length - 1, prev + 1));
+    const nextIdx = Math.min(imageUrls.length - 1, selectedIdx + 1);
+    if (nextIdx === selectedIdx) return;
+    const thumbnail = document.querySelector<HTMLImageElement>(`#${pannelThumbnails} [data-idx='${nextIdx}'] img`);
+    moveToImage(nextIdx, thumbnail);
   };
 
   if (imageUrls.length === 0) return null;
@@ -49,14 +81,13 @@ const PannelImage = ({ postId }: Props) => {
   return (
     <div className={cx(baseStyles.pannel, styles.imagePannel)}>
       <div className={styles.imageWrapper}>
-        <ImageExpandable src={imageUrls[selectedIdx]} alt='' ignoreDimension />
-
-        {imageUrls.length > 1 && (
+        <ImageExpandable src={imageUrls[selectedIdx]} alt='' ignoreDimension summaryId={summaryId} />
+        {imageUrls.length > 1 && selectedIdx > 0 && (
           <button type='button' onClick={onClickPrev} className={cx(styles.arrow, styles.prev)}>
             <IconChevronRight />
           </button>
         )}
-        {imageUrls.length > 1 && (
+        {imageUrls.length > 1 && selectedIdx < imageUrls.length - 1 && (
           <button type='button' onClick={onClickNext} className={cx(styles.arrow, styles.next)}>
             <IconChevronRight />
           </button>
@@ -64,7 +95,7 @@ const PannelImage = ({ postId }: Props) => {
       </div>
 
       {imageUrls.length > 1 && (
-        <ul className={styles.thumbnails}>
+        <ul className={styles.thumbnails} id={pannelThumbnails}>
           {imageUrls.map((url, i) => (
             <li key={url} className={cx({ [styles.selected]: i === selectedIdx })}>
               <button type='button' onClick={onClickThumbnail} data-idx={i}>
