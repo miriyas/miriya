04da342639feda26b78035da8d610e7ff9f84cac
Lee Jun Hyuk <miriya.lee@gmail.com>
2023-06-23 01:22:00 +0900

    ADD: test page for view transition api

---
+++ src/app/test/viewtransitionapi/page.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import { MouseEventHandler, useRef, useState } from 'react';
+import Image from 'next/image';
+import { flushSync } from 'react-dom';
+
+import { transitionHelper } from '@/utils/transition';
+
+import styles from './index.module.scss';
+
+const images = [
+  '/images/blog/og.jpg',
+  '/images/mycar/og.jpg',
+  '/images/idols/og.jpg',
+  '/images/pentax/og.jpg',
+  '/images/minihome/og.jpg',
+  '/images/profile/og.jpg',
+];
+
+const TestPage = () => {
+  const largeImageRef = useRef<HTMLImageElement>(null);
+  const [currentImage, setCurrentImage] = useState('/images/blog/og.jpg');
+
+  const onClick: MouseEventHandler<HTMLImageElement> = (e) => {
+    const thumbnail = e.currentTarget;
+    const newSrc = thumbnail.dataset.url ?? '';
+    largeImageRef.current?.style.setProperty('view-transition-name', '');
+    thumbnail?.style.setProperty('view-transition-name', 'transition-test');
+    transitionHelper({
+      updateDOM() {
+        flushSync(() => {
+          thumbnail?.style.setProperty('view-transition-name', '');
+          largeImageRef.current?.style.setProperty('view-transition-name', 'transition-test');
+          setCurrentImage(newSrc);
+        });
+      },
+    }).finished.finally(() => {});
+  };
+
+  return (
+    <main className={styles.testPage}>
+      <div className={styles.large}>
+        <Image src={currentImage} width={200} height={250} alt='' onClick={onClick} ref={largeImageRef} />
+      </div>
+      <div className={styles.thumbnails}>
+        {images
+          .filter((url) => url !== currentImage)
+          .map((imageUrl) => (
+            <Image
+              key={imageUrl}
+              src={imageUrl}
+              width={200}
+              height={250}
+              alt=''
+              onClick={onClick}
+              data-url={imageUrl}
+            />
+          ))}
+      </div>
+    </main>
+  );
+};
+
+export default TestPage;
