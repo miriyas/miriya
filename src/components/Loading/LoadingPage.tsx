443d3f8896e7b0f2f1f7d67b71ff74e59a57c6c8
Lee Jun Hyuk <miriya.lee@gmail.com>
2023-07-08 04:31:01 +0900

    FIX: Loading cursor with JS

--- src/components/Loading/LoadingPage.tsx
+++ src/components/Loading/LoadingPage.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import cx from 'clsx';
+import { useEffect, useRef } from 'react';
 
 import Loading from '.';
 import styles from './Loading.module.scss';
@@ -11,9 +12,26 @@ interface Props {
 }
 
 const LoadingPage = ({ inComponent, test }: Props) => {
+  const hourGlassCursorRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const onMouseMove = (e: MouseEvent): void => {
+      const cursor = hourGlassCursorRef.current;
+      if (!cursor) return;
+      cursor.style.left = `${e.pageX - 25 / 2}px`;
+      cursor.style.top = `${e.pageY - 25 / 2}px`;
+    };
+
+    window.addEventListener('mousemove', onMouseMove);
+    return () => {
+      window.removeEventListener('mousemove', onMouseMove);
+    };
+  }, []);
+
   return (
     <div className={cx(styles.loadingPage, { [styles.inComponent]: inComponent, [styles.test]: test })}>
       <Loading />
+      <div ref={hourGlassCursorRef} className={styles.hourGlassCursor} />
     </div>
   );
 };
